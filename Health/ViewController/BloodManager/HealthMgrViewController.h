cc7aaac82d9ce4b41ddd3d047b4d211c84c66fe0
shixuning <shixuning@tongqutongqu.cn>
2015-11-19 22:00:16 +0800

    commit code

---
+++ Health/ViewController/BloodManager/HealthMgrViewController.h
@@ -0,0 +1,13 @@
+//
+//  HealthMgrViewController.h
+//  Health
+//
+//  Created by antonio on 15/11/17.
+//  Copyright © 2015年 vickycao1221. All rights reserved.
+//
+
+#import "KBaseViewController.h"
+
+@interface HealthMgrViewController : KBaseViewController
+
+@end
