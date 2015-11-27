3de5156fba137b56f1df4ecc6dca1087de6d21ad
someone <someone@someone.com>
2015-11-27 23:21:27 +0800

    支付失败

---
+++ Health/ViewController/Shop/PayResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayResultViewController.h
+//  Health
+//
+//  Created by VickyCao on 11/27/15.
+//  Copyright © 2015 vickycao1221. All rights reserved.
+//
+
+#import "KBaseViewController.h"
+
+@interface PayResultViewController : KBaseViewController
+
+@end
---
+++ Health/ViewController/Shop/PayResultViewController.m
@@ -0,0 +1,13 @@
+//
+//  PayResultViewController.m
+//  Health
+//
+//  Created by VickyCao on 11/27/15.
+//  Copyright © 2015 vickycao1221. All rights reserved.
+//
+
+#import "PayResultViewController.h"
+
+@implementation PayResultViewController
+
+@end
